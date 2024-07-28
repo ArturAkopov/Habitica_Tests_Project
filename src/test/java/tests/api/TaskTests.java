f0aff168bcd20b681c33bdfa88eb014765ac45a5
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-28 23:30:38 +0300

    Тесты для взаимодействия с задачами

---
+++ src/test/java/tests/api/TaskTests.java
@@ -0,0 +1,38 @@
+package tests.api;
+
+import authorization.Authorization;
+import authorization.model.AuthResponseBodyModel;
+import model.GetUserResponseBodyModel;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static spec.HabiticaSpec.requestSpec;
+import static spec.HabiticaSpec.responseSpec200;
+
+@Tag("api")
+@DisplayName("Проверки взаимодействия с задачами")
+public class TaskTests extends TestBase {
+
+    TestSteps steps = new TestSteps();
+
+    @Test
+    @DisplayName("Проверка успешного получения информации о списках задач")
+    void getUserTasksTest() {
+        AuthResponseBodyModel authResponse = steps.getAuthorization();
+        GetUserResponseBodyModel response = given(requestSpec)
+                .when()
+                .header("X-Api-Key", authResponse.getData().getApiToken())
+                .header("X-Api-User", authResponse.getData().getId())
+                .get("/user")
+                .then()
+                .spec(responseSpec200)
+                .extract().as(GetUserResponseBodyModel.class);
+        Assertions.assertThat(response.getData().getTasksOrder().getDailys()).isEmpty();
+        Assertions.assertThat(response.getData().getTasksOrder().getTodos()).isNotEmpty();
+
+    }
+
+}
