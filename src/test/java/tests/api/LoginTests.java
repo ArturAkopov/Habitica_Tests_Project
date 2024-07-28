c479d034471d61513eb0fbcf83da19041be9ccb6
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-28 23:29:19 +0300

    Тесты на авторизацию

---
+++ src/test/java/tests/api/LoginTests.java
@@ -0,0 +1,34 @@
+package tests.api;
+
+import authorization.Authorization;
+import authorization.model.AuthResponseBodyModel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("api")
+@DisplayName("Проверки авторизации")
+public class LoginTests extends TestBase {
+
+    TestSteps steps = new TestSteps();
+
+    @Test
+    @DisplayName("Проверка корректной авторизации")
+    void authorizationSuccessTest() {
+        AuthResponseBodyModel response = steps.getAuthorization();
+        steps.checkStatusForAuth(response);
+        steps.checkUserIdForAuth(response);
+        steps.checkApiTokenForAuth(response);
+        steps.checkUserNameForAuth(response);
+    }
+
+    @Test
+    @DisplayName("Проверка невозможности авторизации без пароля")
+    void authorizationNoSuccessTest() {
+        AuthResponseBodyModel response = steps.getBadAuthResponse();
+        steps.checkBadStatusForAuth(response);
+        steps.checkMessageErrorForAuth(response);
+        steps.checkClarifyingMessageErrorsForAuth(response);
+    }
+
+}
