039e6fcdf45355965360f2d00e2eeb7b4b40cd7f
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-28 23:28:32 +0300

    Спецификация АПИ запросов

---
+++ src/test/java/spec/HabiticaSpec.java
@@ -0,0 +1,28 @@
+package spec;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.http.ContentType.JSON;
+
+public class HabiticaSpec {
+
+    public static RequestSpecification requestSpec = with()
+            .filter(withCustomTemplates())
+            .contentType(JSON)
+            .log().all();
+
+    public static ResponseSpecification responseSpec200 = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static ResponseSpecification responseSpec400 = new ResponseSpecBuilder()
+            .expectStatusCode(400)
+            .log(LogDetail.ALL)
+            .build();
+}
