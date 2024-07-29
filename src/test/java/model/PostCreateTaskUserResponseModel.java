1012e27e01f538ce25499346fceebdbb42d7964b
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-29 20:17:25 +0300

    Модель ответа на создание задачи

---
+++ src/test/java/model/PostCreateTaskUserResponseModel.java
@@ -0,0 +1,20 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PostCreateTaskUserResponseModel {
+    private Boolean success;
+    private Data data;
+
+    @lombok.Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Data {
+        private String type;
+        private String text;
+        private String _id;
+
+    }
+}
