e5d56011559e6a8d4c326b57cee459086e252194
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-29 11:43:07 +0300

    Рефакторинг Получения списка задач

--- src/test/java/model/GetUserResponseBodyModel.java
+++ src/test/java/model/GetUserResponseBodyModel.java
@@ -3,11 +3,27 @@ package model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.util.ArrayList;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GetUserResponseBodyModel {
-
-    private boolean success;
+    private Boolean success;
     private GetUserBodyData data;
 
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GetUserBodyData {
+        private GetUserBodyTasksOrder tasksOrder;
+        private String id;
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class GetUserBodyTasksOrder {
+            private ArrayList<Object> habits;
+            private ArrayList<Object> dailys;
+            private ArrayList<Object> todos;
+            private ArrayList<Object> rewards;
+        }
+    }
 }
