e010535c2421421c79b03912c77257fd4dcf99d6
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-29 20:18:02 +0300

    Добавление тестов

--- src/test/java/tests/api/TaskTests.java
+++ src/test/java/tests/api/TaskTests.java
@@ -1,10 +1,16 @@
 package tests.api;
 
 import authorization.model.AuthResponseBodyModel;
+import model.DeleteTaskUserResponseBodyModel;
 import model.GetUserResponseBodyModel;
+import model.PostCreateTaskUserResponseModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import tests.TestData;
+
+import java.io.IOException;
+
 
 
 @Tag("api")
@@ -12,6 +18,9 @@ import org.junit.jupiter.api.Test;
 public class TaskTests extends TestBase {
 
     TestSteps steps = new TestSteps();
+    TestData testData = new TestData();
+    String text = testData.taskName;
+    String id = testData.taskId;
 
     @Test
     @DisplayName("Проверка успешного получения информации о списках задач")
@@ -19,6 +28,23 @@ public class TaskTests extends TestBase {
         AuthResponseBodyModel authResponse = steps.getAuthorization();
         GetUserResponseBodyModel response = steps.getUserTasks(authResponse);
         steps.checkTasksOrderList(authResponse,response);
+    }
+
+    @Test
+    @DisplayName("Проверка создания новой задачи")
+    void createUserTaskTest() throws IOException {
+        AuthResponseBodyModel authResponse = steps.getAuthorization();
+        PostCreateTaskUserResponseModel response = steps.createNewTask(authResponse,text,id);
+        steps.checkSuccessfulCreateTask(response,text,id);
+    }
+
+    @Test
+    @DisplayName("Проверка удаления задачи")
+    void deleteUserTaskTest() throws IOException {
+        AuthResponseBodyModel authResponse = steps.getAuthorization();
+        PostCreateTaskUserResponseModel taskResponse = steps.createNewTask(authResponse,text,id);
+        DeleteTaskUserResponseBodyModel response = steps.deleteUserTask(authResponse,taskResponse.getData().get_id());
+        steps.checkStatus(response.getSuccess());
     }
 
 }
