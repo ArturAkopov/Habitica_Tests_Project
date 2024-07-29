ad07f38474875149433c53f4418654940563c54b
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-29 20:18:16 +0300

    Тестовые данные

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,14 @@
+package tests;
+
+import com.github.javafaker.Faker;
+
+import java.util.UUID;
+
+public class TestData {
+
+        Faker faker = new Faker();
+
+        public String taskName = faker.funnyName().name();
+        public String taskId = UUID.randomUUID().toString();
+
+}
