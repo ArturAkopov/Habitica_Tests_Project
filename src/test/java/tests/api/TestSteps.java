28800db46df294010dfbe3ad3f2830182f3075b3
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-29 20:18:25 +0300

    Добавлены тестовые шаги

--- src/test/java/tests/api/TestSteps.java
+++ src/test/java/tests/api/TestSteps.java
@@ -3,11 +3,18 @@ package tests.api;
 import authorization.Authorization;
 import authorization.model.AuthRequestBodyModel;
 import authorization.model.AuthResponseBodyModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import config.AuthConfig;
 import io.qameta.allure.Step;
+import model.DeleteTaskUserResponseBodyModel;
 import model.GetUserResponseBodyModel;
+import model.PostCreateTaskUserRequestModel;
+import model.PostCreateTaskUserResponseModel;
 import org.aeonbits.owner.ConfigFactory;
 import org.assertj.core.api.Assertions;
+
+import java.io.File;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 import static spec.HabiticaSpec.*;
@@ -15,6 +22,7 @@ import static spec.HabiticaSpec.*;
 public class TestSteps {
 
     AuthConfig authConfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
+
 
     @Step("Авторизация пользователя")
     public AuthResponseBodyModel getAuthorization() {
@@ -39,7 +47,7 @@ public class TestSteps {
 
     @Step("Получение задач для пользователя {authResponse.data.username}")
     public GetUserResponseBodyModel getUserTasks(AuthResponseBodyModel authResponse) {
-        return  given(requestSpec)
+        return given(requestSpec)
                 .when()
                 .header("X-Api-Key", authResponse.getData().getApiToken())
                 .header("X-Api-User", authResponse.getData().getId())
@@ -49,24 +57,58 @@ public class TestSteps {
                 .extract().as(GetUserResponseBodyModel.class);
     }
 
-    @Step("Проверка статуса success = {response.success} для неуспешной авторизации")
+    @Step("Создание новой задачи для пользователя {authResponse.data.username}")
+    public PostCreateTaskUserResponseModel createNewTask(AuthResponseBodyModel authResponse, String setText, String setId) throws IOException {
+
+        ObjectMapper om = new ObjectMapper();
+        PostCreateTaskUserRequestModel[] request = om.readValue(
+                new File("src/test/resources/request/PostCreateTaskUser.json"),
+                PostCreateTaskUserRequestModel[].class
+        );
+        request[0].setText(setText);
+        request[0].set_id(setId);
+
+        return given(requestSpec)
+                .when()
+                .body(request)
+                .header("X-Api-Key", authResponse.getData().getApiToken())
+                .header("X-Api-User", authResponse.getData().getId())
+                .post("/tasks/user")
+                .then()
+                .spec(responseSpec201)
+                .extract().as(PostCreateTaskUserResponseModel.class);
+    }
+
+    @Step("Удаление задачи {id} для пользователя {authResponse.data.username}")
+    public DeleteTaskUserResponseBodyModel deleteUserTask(AuthResponseBodyModel authResponse,String id) {
+        return given(requestSpec)
+                .when()
+                .header("X-Api-Key", authResponse.getData().getApiToken())
+                .header("X-Api-User", authResponse.getData().getId())
+                .get("/tasks/"+id)
+                .then()
+                .spec(responseSpec200)
+                .extract().as(DeleteTaskUserResponseBodyModel.class);
+    }
+
+    @Step("Проверка статуса success = false для неуспешной авторизации")
     public void checkBadStatusForAuth(AuthResponseBodyModel response) {
         Assertions.assertThat(response.getSuccess()).isEqualTo(false);
     }
 
-    @Step("Проверка сообщения об ошибке message = {response.message} для неуспешной авторизации")
+    @Step("Проверка сообщения об ошибке message = \"Invalid request parameters.\" для неуспешной авторизации")
     public void checkMessageErrorForAuth(AuthResponseBodyModel response) {
         Assertions.assertThat(response.getMessage()).contains("Invalid request parameters.");
     }
 
-    @Step("Проверка уточняющего сообщения об ошибке errors.message = {response.errors.message} для неуспешной авторизации")
+    @Step("Проверка уточняющего сообщения об ошибке errors.message = \"Missing password.\" для неуспешной авторизации")
     public void checkClarifyingMessageErrorsForAuth(AuthResponseBodyModel response) {
         Assertions.assertThat(response.getErrors().get(0).getMessage()).contains("Missing password.");
     }
 
-    @Step("Проверка статуса success = {response.success} для успешной авторизации")
-    public void checkStatusForAuth(AuthResponseBodyModel response) {
-        Assertions.assertThat(response.getSuccess()).isEqualTo(true);
+    @Step("Проверка статуса success = true ")
+    public void checkStatus(Boolean status) {
+        Assertions.assertThat(status).isEqualTo(true);
     }
 
     @Step("Проверка id = {response.data.id} авторизованного пользователя")
@@ -92,5 +134,12 @@ public class TestSteps {
         Assertions.assertThat(response.getData().getTasksOrder().getTodos()).isNotNull();
         Assertions.assertThat(response.getData().getTasksOrder().getDailys()).isNotNull();
         Assertions.assertThat(response.getData().getTasksOrder().getHabits()).isNotNull();
+    }
+
+    @Step("Проверка успешного создания задачи")
+    public void checkSuccessfulCreateTask(PostCreateTaskUserResponseModel response, String setText, String setId) {
+        Assertions.assertThat(response.getSuccess()).isTrue();
+        Assertions.assertThat(response.getData().getText()).isEqualTo(setText);
+        Assertions.assertThat(response.getData().get_id()).isEqualTo(setId);
     }
 }
