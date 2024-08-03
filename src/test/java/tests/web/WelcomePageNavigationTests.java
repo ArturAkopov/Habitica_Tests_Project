5da4b1323eb9fb19551924390fe31e2aadc85741
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-03 21:56:15 +0300

    Тесты на кнопки навигации приветственной страницы

---
+++ src/test/java/tests/web/WelcomePageNavigationTests.java
@@ -0,0 +1,33 @@
+package tests.web;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import tests.web.pages.WelcomePage;
+
+
+@Tag("web")
+@Owner("Акопов Артур")
+@Feature("WelcomeScreenNavigation")
+public class WelcomePageNavigationTests extends WebTestBase {
+
+    WelcomePage welcomePage = new WelcomePage();
+
+    @Test
+    @DisplayName("\"Проверка действия кнопки перехода к авторизации - \"Вход\"")
+    void checkEnterButtonTest() {
+        welcomePage.openWelcomePage()
+                .clickEnterButton()
+                .checkOpenScreenWithRequiredUrl("https://habitica.com/login");
+    }
+
+    @Test
+    @DisplayName("\"Проверка действия кнопки перехода к регистрации - \"Регистрация\"")
+    void checkJoinButtonTest() {
+        welcomePage.openWelcomePage()
+                .clickJoinButton()
+                .checkOpenScreenWithRequiredUrl("https://habitica.com/register");
+    }
+}
