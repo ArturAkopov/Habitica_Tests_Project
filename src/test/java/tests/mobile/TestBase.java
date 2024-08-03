4e0b7e0338b4943f1a02db6ec7a2416b1ef104a5
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-03 12:52:35 +0300

    Базовый класс для мобильных тестов

--- src/test/java/tests/mobile/TestBase.java
+++ src/test/java/tests/mobile/TestBase.java
@@ -2,46 +2,40 @@ package tests.mobile;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.junit.jupiter.api.BeforeEach;
+import tests.mobile.drivers.RealMobileDriver;
 
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
+
+
     @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = "https://habitica.com/";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("size", "1920x1080");
-        Configuration.browserVersion = System.getProperty("version","121");
+    static void beforeAll() {
+        Configuration.browserSize = null;
+//        if (System.getProperty("deviceHost", "browserstack").equals("real")) {
+            Configuration.browser = RealMobileDriver.class.getName();
+//            ;}
+//        } else if (System.getProperty("deviceHost", "browserstack").equals("emulation")) {
+//            Configuration.browser = EmulateMobileDriver.class.getName();
+//        } else if (System.getProperty("deviceHost", "browserstack").equals("browserstack")) {
+//            Configuration.browser = BrowserStackDriver.class.getName();
+//        }
+    }
 
-        if (System.getProperty("host","local").equals("remote")) {
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;}
-
-        RestAssured.baseURI = "https://habitica.com/";
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
     }
 
     @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        if (System.getProperty("host","local").equals("remote")) {Attach.addVideo();}
+    void afterEach() {
         closeWebDriver();
     }
 }
