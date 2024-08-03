c36a02e27f20555d51869af950d1e1cbce2550fc
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 00:07:32 +0300

    Массив элементов меню на главной странице

---
+++ src/test/java/utils/MainPageMenuItems.java
@@ -0,0 +1,22 @@
+package utils;
+
+import lombok.Getter;
+
+@Getter
+public enum MainPageMenuItems {
+    ISSUES(0, "https://habitica.com/"),
+    INVENTORY(1, "https://habitica.com/inventory/items"),
+    SHOP(2, "https://habitica.com/shops/market"),
+    TEAM(3, "https://habitica.com/party"),
+    GROUP(4, "https://habitica.com/group-plans"),
+    CHALLENGES(5, "https://habitica.com/challenges/myChallenges"),
+    FAQ(6, "https://habitica.com/static/faq");
+
+    private final int value;
+    private final String link;
+
+    MainPageMenuItems(int value, String link) {
+        this.value = value;
+        this.link = link;
+    }
+}
