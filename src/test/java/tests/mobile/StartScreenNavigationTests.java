ba7637569e684a53726e8cb01d3a75c839b0d79b
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-03 13:46:09 +0300

    Тесты на навигацию по начальным экранам

--- src/test/java/tests/mobile/StartScreenNavigationTests.java
+++ src/test/java/tests/mobile/StartScreenNavigationTests.java
@@ -5,6 +5,9 @@ import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import tests.mobile.screens.AuthorisationScreen;
+import tests.mobile.screens.MainNavigationScreen;
+import tests.mobile.screens.RegistrationScreen;
 import tests.mobile.screens.WelcomeScreen;
 
 @Tag("mobile")
@@ -13,14 +16,35 @@ import tests.mobile.screens.WelcomeScreen;
 @DisplayName("Проверки кнопок навигации начальных экранов")
 public class StartScreenNavigationTests extends TestBase {
 
+    WelcomeScreen welcomeScreen = new WelcomeScreen();
+    MainNavigationScreen mainNavigationScreen = new MainNavigationScreen();
+    AuthorisationScreen authorisationScreen = new AuthorisationScreen();
+    RegistrationScreen registrationScreen = new RegistrationScreen();
+
     @Test
     @DisplayName("Проверка кнопки пропуска приветственного экрана - \"skip\"")
-    void checkIntroScreenTest() {
-
-        WelcomeScreen welcomeScreen = new WelcomeScreen();
-
+    void checkSkipButton() {
         welcomeScreen.
                 clickSkipButton().
                 checkAbsenceWelcomeMessage();
     }
+
+    @Test
+    @DisplayName("Проверка действия кнопки перехода к авторизации - \"LOGIN\"")
+    void checkLoginButton() {
+        welcomeScreen.clickSkipButton();
+        mainNavigationScreen.clickLoginButton();
+        authorisationScreen.checkVisibleAuthorisationFields();
+
+    }
+
+    @Test
+    @DisplayName("Проверка действия кнопки перехода к регистрации - \"REGISTER\"")
+    void checkRegisterButton() {
+        welcomeScreen.clickSkipButton();
+        mainNavigationScreen.clickRegisterButton();
+        registrationScreen.checkVisibleRegistrationFields();
+
+    }
+
 }
