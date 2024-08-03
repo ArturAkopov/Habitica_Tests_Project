07705e384957ec98c9152212fc7074f5c519cabc
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 00:07:08 +0300

    Тесты для главной страницы

---
+++ src/test/java/tests/web/MainPageTests.java
@@ -0,0 +1,31 @@
+package tests.web;
+
+import extensions.WithLogin;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import tests.web.pages.MainPage;
+import utils.MainPageMenuItems;
+
+@Tag("web")
+@Owner("Акопов Артур")
+@Feature("User actions on the main page")
+@DisplayName("Проверки действий пользователя на главной странице")
+public class MainPageTests extends WebTestBase {
+
+    MainPage mainPage = new MainPage();
+    MainPageMenuItems[] mainPageChapters = MainPageMenuItems.values();
+
+    @Test
+    @WithLogin
+    @DisplayName("Проверка перемещения по разделам главной страницы")
+    void mainPageNavigationTest() {
+        mainPage.openMainPage();
+        for (MainPageMenuItems item : mainPageChapters) {
+            mainPage.clickMenuElement(item.getValue())
+                    .checkCurrentUrl(item.getLink());
+        }
+    }
+}
