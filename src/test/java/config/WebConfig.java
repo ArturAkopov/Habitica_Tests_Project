86b310f1af5be15efdf2cc2ce1ce4b57acbdd807
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-03 21:15:05 +0300

    Конфиги для веба

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+
+import org.aeonbits.owner.Config;
+
+
+public interface WebConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://habitica.com")
+    String baseUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String browser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("browserVersion")
+    @DefaultValue("100")
+    String browserVersion();
+
+    @Key("remote")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String remote();
+
+}
