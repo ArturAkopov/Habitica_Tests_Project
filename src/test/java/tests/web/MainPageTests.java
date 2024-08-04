b1ad81bf7dc8227c8f8843e28b85863258d148ac
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 14:59:07 +0300

    Добавлены тесты

--- src/test/java/tests/web/MainPageTests.java
+++ src/test/java/tests/web/MainPageTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.web.pages.MainPage;
 import utils.MainPageMenuItems;
+import utils.TestData;
 
 @Tag("web")
 @Owner("Акопов Артур")
@@ -15,6 +16,9 @@ import utils.MainPageMenuItems;
 @DisplayName("Проверки действий пользователя на главной странице")
 public class MainPageTests extends WebTestBase {
 
+    TestData data = new TestData();
+    String habitName = data.taskName;
+    String taskName = data.taskName;
     MainPage mainPage = new MainPage();
     MainPageMenuItems[] mainPageChapters = MainPageMenuItems.values();
 
@@ -28,4 +32,23 @@ public class MainPageTests extends WebTestBase {
                     .checkCurrentUrl(item.getLink());
         }
     }
+
+    @Test
+    @WithLogin
+    @DisplayName("Проверка быстрого создания задачи на главной странице")
+    void quickCreateHabitTest() {
+        mainPage.openMainPage()
+                .createQuickHabit(habitName)
+                .checkVisibleHabit(habitName);
+    }
+
+    @Test
+    @WithLogin
+    @DisplayName("Проверка быстрого создания задачи на главной странице")
+    void quickCreateTaskTest() {
+        mainPage.openMainPage()
+                .createQuickTask(taskName)
+                .checkVisibleTask(taskName);
+    }
+
 }
