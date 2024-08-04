f89b17621ba2b4f6d4a17a1f73207565c0a31bc3
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 16:37:30 +0300

    Добавлена спецификация

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -4,6 +4,8 @@ import config.AuthConfig;
 import org.aeonbits.owner.ConfigFactory;
 
 import static io.restassured.RestAssured.given;
+import static spec.HabiticaSpec.requestSpec;
+import static spec.HabiticaSpec.responseSpec200;
 
 public class Browserstack {
     static final AuthConfig config = ConfigFactory.create(AuthConfig.class, System.getProperties());
@@ -11,13 +13,11 @@ public class Browserstack {
     public static String videoUrl(String sessionId) {
         String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
-        return given()
+        return given(requestSpec)
                 .auth().basic(config.browserstackUser(), config.browserstackPassword())
                 .get(url)
                 .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
+                .spec(responseSpec200)
                 .extract().path("automation_session.video_url");
     }
 }
