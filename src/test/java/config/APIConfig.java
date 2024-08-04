a4cd0e3237cb14980f7a35902269c22ba3a9dc38
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 15:21:58 +0300

    Изменены конфиги

--- src/test/java/config/APIConfig.java
+++ src/test/java/config/APIConfig.java
@@ -2,12 +2,14 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({
-        "classpath:api.properties"
-})
+
 public interface APIConfig extends Config {
+
+    @Key("baseURI")
     @DefaultValue("https://habitica.com")
     String baseURI();
+
+    @Key("basePath")
     @DefaultValue("/api/v4")
     String basePath();
 }
