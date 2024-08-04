fee59c78f97dcde27b8c3edcefb528c7b010cade
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-08-04 14:59:35 +0300

    Убрана лишняя зависимость

--- src/test/java/tests/web/pages/MainPage.java
+++ src/test/java/tests/web/pages/MainPage.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.currentFrameUrl;
